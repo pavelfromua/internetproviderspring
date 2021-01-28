7e11a775ce2e7b9e9e7ae690c5e848a3b2b85971
nglqhYHj <pavelfromua@gmail.com>
2021-01-28 10:00:32 +0200

    init commit

---
+++ src/main/java/my/project/internetprovider/InternetproviderApplication.java
@@ -0,0 +1,13 @@
+package my.project.internetprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetproviderApplication.class, args);
+	}
+
+}
